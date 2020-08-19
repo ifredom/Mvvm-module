7ae6129616d0c630510b89e0408f72ca3530c3fa
ifredom <1950735817@qq.com>
2020-08-19 23:02:40 +0800

    写MVVM (3.1)

--- observer.js
+++ observer.js
@@ -6,14 +6,18 @@ class Observer {
   // 参数代理, 实现 vm.xxx -> vm._data.xxx
   proxyData(vm, data) {
     var that = vm;
+    var dep = new Dep();
+    
     Object.keys(data).forEach((key) => {
       Object.defineProperty(that, key, {
         get() {
           return that._data[key];
         },
         set(newValue) {
-          that._data[key] = newValue;
-          that.el.innerHTML = that._data[key]; // 更改模板数据的值
+          dep.sendMessage();
+          // that._data[key] = newValue;
+          // that.el.innerHTML = that._data[key];
+          var compiler = new Compiler(vm, newValue, key);
         },
         enumerable: true,
         configurable: true,
@@ -30,29 +34,6 @@ class Dep {
     this.allMessage.push(oneMessage);
   }
   sendMessage() {
-    console.log("发个消息");
     this.allMessage.forEach((sub) => sub.getMessage());
   }
 }
-
-// 创建
-var dep = new Dep();
-
-var xiaoming = {
-  getMessage() {
-    console.log("我，小明，收到消息了");
-  },
-};
-var xiaohong = {
-  getMessage() {
-    console.log("我，小红，收到消息了");
-  },
-};
-
-dep.addFollower(xiaoming);
-dep.addFollower(xiaoming);
-
-dep.sendMessage();
-// >> 发个消息
-// >> 我，小明，收到消息了
-// >> 我，小红，收到消息了
